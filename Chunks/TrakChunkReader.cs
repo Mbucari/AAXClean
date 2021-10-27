83ef0f326af1a1bf650c49d5644f45d5fe9e6d57
Michael Bucari-Tovo <mbucari1@gmail.com>
2021-10-27 15:47:56 -0600

    Handle cases where chunk offset table is not in order.

--- Chunks/TrakChunkReader.cs
+++ Chunks/TrakChunkReader.cs
@@ -21,10 +21,12 @@ namespace AAXClean.Chunks
 		{
 			InputStream = file;
 
-			List<(long chunkOffset, ChunkTable)> chunkList = new();
+			List<(long chunkOffset, ChunkTable)> combinedChunkList = new();
 
 			foreach (var handler in handlers)
 			{
+				List<(long chunkOffset, ChunkTable)> chunkList = new();
+
 				var chunkTable = new ChunkTable(handler.Track.Mdia.Minf.Stbl, handler);
 
 				if (handler.Track.Mdia.Minf.Stbl.Stco is null)
@@ -49,11 +51,15 @@ namespace AAXClean.Chunks
 
 					}
 				}
+
+				chunkTable.SetChunkOffsets(chunkList.Select(c => c.chunkOffset).ToList());
+
+				combinedChunkList.AddRange(chunkList);
 			}
 
-			chunkList.Sort((o1, o2) => o1.chunkOffset.CompareTo(o2.chunkOffset));
+			combinedChunkList.Sort((o1, o2) => o1.chunkOffset.CompareTo(o2.chunkOffset));
 
-			ChunkQueue = new Queue<(long, ChunkTable)>(chunkList);
+			ChunkQueue = new Queue<(long, ChunkTable)>(combinedChunkList);
 		}
 
 		/// <summary>
@@ -82,33 +88,57 @@ namespace AAXClean.Chunks
 					throw new Exception($"Input stream position 0x{InputStream.Position:X8} is past the chunk offset 0x{chunkOffset:X8} and is not seekable.");
 			}
 
-			(int totalChunkSize, uint frameIndex, int[] frameSizes) = table.GetNextChunk();
+			(int totalChunkSize, uint frameIndex, int[] frameSizes) = table.GetChunkFrames(chunkOffset);
 
 			Span<byte> chunkBuffer = new byte[totalChunkSize];
 			InputStream.Read(chunkBuffer);
 
 
-			return table.Handler.ChunkAvailable(chunkBuffer, table.NextChunkIndex - 1, frameIndex, totalChunkSize, frameSizes);
+			return table.Handler.ChunkAvailable(chunkBuffer, table.CurrentChunkIndex, frameIndex, totalChunkSize, frameSizes);
 		}
 
 		private class ChunkTable
 		{
 			public IChunkHandler Handler { get; }
-			public uint NextChunkIndex { get; set; } = 0;
+			public uint CurrentChunkIndex { get; private set; } = 0;
 			private int[] SampleSizes { get; }
-			private StscBox.ChunkEntry[] ChunkEntries { get; }
+			private StscBox.ChunkEntry[] SamplesToChunks { get; }
+
+			private Dictionary<long, uint> ChunkOffsetToChunkIndex;
 			public ChunkTable(StblBox stbl, IChunkHandler handler)
 			{
 				Handler = handler;
 
 				SampleSizes = stbl.Stsz.SampleSizes.ToArray();
 
-				ChunkEntries = stbl.Stsc.Samples.ToArray();
+				SamplesToChunks = stbl.Stsc.Samples.ToArray();
 			}
 
-			public (int totalChunkSize, uint frameIndex, int[] frameSizes) GetNextChunk()
+			/// <summary>
+			/// Create a map of chunk offsets to chunk indices. This is necessary because some Audible titles
+			/// (eg. Broken Angels: B002V8H59I) have the chunk offsets out of order. <see cref="TrakChunkReader"/>
+			/// reads sorts chunk offsets and reads them in order, but we need the chunk offset INDEX in order
+			/// to calculate the frame index and frame sizes.
+			/// </summary>
+			public void SetChunkOffsets(List<long> chunkOffsets)
 			{
-				(uint firstFrame, uint numFrames) = NumSamplesInChunk(NextChunkIndex++);
+				ChunkOffsetToChunkIndex = new();
+
+				for (int i = 0; i < chunkOffsets.Count; i++)
+				{
+					ChunkOffsetToChunkIndex[chunkOffsets[i]] = (uint)i;
+				}
+			}
+
+			public (int totalChunkSize, uint frameIndex, int[] frameSizes) GetChunkFrames(long chunkOffset)
+			{
+				return GetChunkFrames(ChunkOffsetToChunkIndex[chunkOffset]);
+			}
+
+			public (int totalChunkSize, uint frameIndex, int[] frameSizes) GetChunkFrames(uint chunkIndex)
+			{
+				CurrentChunkIndex = chunkIndex;
+				(uint firstFrame, uint numFrames) = NumSamplesInChunk(chunkIndex);
 
 				int[] frameSizes = new int[numFrames];
 				int totalChunkSize = 0;
@@ -137,15 +167,15 @@ namespace AAXClean.Chunks
 
 				uint firstFrame = 0, numFrames = 0;
 
-				for (int i = 0; i < ChunkEntries.Length; i++)
+				for (int i = 0; i < SamplesToChunks.Length; i++)
 				{
-					if (i + 1 == ChunkEntries.Length || chunk < ChunkEntries[i + 1].FirstChunk)
+					if (i + 1 == SamplesToChunks.Length || chunk < SamplesToChunks[i + 1].FirstChunk)
 					{
-						firstFrame += (chunk - ChunkEntries[i].FirstChunk) * ChunkEntries[i].SamplesPerChunk;
-						numFrames = ChunkEntries[i].SamplesPerChunk;
+						firstFrame += (chunk - SamplesToChunks[i].FirstChunk) * SamplesToChunks[i].SamplesPerChunk;
+						numFrames = SamplesToChunks[i].SamplesPerChunk;
 						break;
 					}
-					firstFrame += (ChunkEntries[i + 1].FirstChunk - ChunkEntries[i].FirstChunk) * ChunkEntries[i].SamplesPerChunk;
+					firstFrame += (SamplesToChunks[i + 1].FirstChunk - SamplesToChunks[i].FirstChunk) * SamplesToChunks[i].SamplesPerChunk;
 				}
 
 				return (firstFrame, numFrames);
