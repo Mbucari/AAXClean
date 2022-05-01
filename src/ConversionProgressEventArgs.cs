46a4d80062b954df0505883462d4b0209b958bc7
Michael Bucari-Tovo <mbucari1@gmail.com>
2022-05-01 11:33:07 -0600

    Add TotalDuration to args

--- src/ConversionProgressEventArgs.cs
+++ src/ConversionProgressEventArgs.cs
@@ -4,12 +4,8 @@ namespace AAXClean
 {
 	public class ConversionProgressEventArgs : EventArgs
 	{
-		internal ConversionProgressEventArgs(TimeSpan position, double speed)
-		{
-			ProcessPosition = position;
-			ProcessSpeed = speed;
-		}
-		public TimeSpan ProcessPosition { get; }
-		public double ProcessSpeed { get; }
+		public TimeSpan ProcessPosition { get; internal init; }
+		public TimeSpan TotalDuration { get; internal init; }
+		public double ProcessSpeed { get; internal init; }
 	}
 }
